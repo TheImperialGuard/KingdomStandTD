d1e54ec395f38ea41b46e54b3550cbad5da696ce
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-05-27 16:24:45 +0300

    feat: Add game entry point logic

---
+++ Assets/_Project/Develop/Runtime/Infrastructure/Boot/GameEntryPoint.cs
@@ -0,0 +1,51 @@
+﻿using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Infrastructure.EntryPoint;
+using Assets._Project.Develop.Runtime.Utilities.ConfigsManagment;
+using Assets._Project.Develop.Runtime.Utilities.CoroutinesManagment;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Infrastructure.Boot
+{
+    public class GameEntryPoint : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log("Старт проекта, сетап настроек");
+
+            SetupAppSettings();
+
+            Debug.Log("Регистрация сервисов глобального контекста");
+
+            DIContainer projectContainer = new();
+
+            ProjectContextRegistrations.Process(projectContainer);
+
+            projectContainer.Initialize();
+
+            projectContainer.Resolve<ICoroutinesPerformer>().StartPerform(Initialize(projectContainer));
+        }
+
+        private IEnumerator Initialize(DIContainer container)
+        {
+            //Show loading screen
+
+            Debug.Log("Начинается инициализация сервисов");
+
+            yield return container.Resolve<ConfigsProviderService>().LoadAsync();
+
+            yield return new WaitForSeconds(1);
+
+            Debug.Log("Завершается инициализация сервисов");
+
+            //Hide loading screen
+            //Switch scene
+        }
+
+        private void SetupAppSettings()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 60;
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Infrastructure/Boot/ProjectContextRegistrations.cs
@@ -0,0 +1,41 @@
+﻿using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Utilities.AssetsManagment;
+using Assets._Project.Develop.Runtime.Utilities.ConfigsManagment;
+using Assets._Project.Develop.Runtime.Utilities.CoroutinesManagment;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Infrastructure.EntryPoint
+{
+    public class ProjectContextRegistrations
+    {
+        public static void Process(DIContainer container)
+        {
+            container.RegisterAsSingle<ICoroutinesPerformer>(CreateCoroutinesPerformer);
+
+            container.RegisterAsSingle(CreateResourcesAssetsLoader);
+
+            container.RegisterAsSingle(CreateConfigsProviderService);
+        }
+
+        private static ResourcesAssetsLoader CreateResourcesAssetsLoader(DIContainer c) => new();
+
+        private static ConfigsProviderService CreateConfigsProviderService(DIContainer c)
+        {
+            ResourcesAssetsLoader resourcesAssetsLoader = c.Resolve<ResourcesAssetsLoader>();
+
+            ResourcesConfigsLoader resourcesConfigsLoader = new(resourcesAssetsLoader);
+
+            return new ConfigsProviderService(resourcesConfigsLoader);
+        }
+
+        private static CoroutinesPerformer CreateCoroutinesPerformer(DIContainer c)
+        {
+            ResourcesAssetsLoader resourcesAssetsLoader = c.Resolve<ResourcesAssetsLoader>();
+
+            CoroutinesPerformer coroutinesPerformerPrefab = resourcesAssetsLoader
+                .Load<CoroutinesPerformer>("Utilities/CoroutinesPerformer");
+
+            return GameObject.Instantiate(coroutinesPerformerPrefab);
+        }
+    }
+}
