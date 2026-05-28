57a83ed53d9ddab77172d8778ce34c8ff4ba4f65
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-05-28 18:58:59 +0300

    feat: Add main menu UI root

--- Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuContextRegistrations.cs
+++ Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuContextRegistrations.cs
@@ -1,4 +1,7 @@
 ﻿using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.UI.Meta.MainMenu;
+using Assets._Project.Develop.Runtime.Utilities.AssetsManagment;
+using UnityEngine;
 
 namespace Assets._Project.Develop.Runtime.Meta.Infrastructure
 {
@@ -6,7 +9,17 @@ namespace Assets._Project.Develop.Runtime.Meta.Infrastructure
     {
         public static void Process(DIContainer container)
         {
+            container.RegisterAsSingle(CreateMainMenuUIRoot).NonLazy();
+        }
 
+        private static MainMenuUIRoot CreateMainMenuUIRoot(DIContainer c)
+        {
+            ResourcesAssetsLoader resourcesAssetsLoader = c.Resolve<ResourcesAssetsLoader>();
+
+            MainMenuUIRoot mainMenuUIRootPrefab = resourcesAssetsLoader
+                .Load<MainMenuUIRoot>("UI/Meta/MainMenu/MainMenuUIRoot");
+
+            return GameObject.Instantiate(mainMenuUIRootPrefab);
         }
     }
 }
