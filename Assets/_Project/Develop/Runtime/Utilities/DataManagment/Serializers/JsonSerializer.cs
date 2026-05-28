29934bb57255a9842b2f00c1c9578dc8f8dc865c
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-05-28 15:46:44 +0300

    feat: Add DataSerializers abstraction and Json realization

---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagment/Serializers/JsonSerializer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Utilities.DataManagment.Serializers
+{
+    public class JsonSerializer : IDataSerializer
+    {
+        public TData Deserialize<TData>(string serializedData)
+        {
+            return JsonConvert.DeserializeObject<TData>(serializedData, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto,
+            });
+        }
+
+        public string Serialize<TData>(TData data)
+        {
+            Formatting formattingMode = Application.isEditor ? Formatting.Indented : Formatting.None;
+
+            return JsonConvert.SerializeObject(data, new JsonSerializerSettings
+            {
+                Formatting = formattingMode,
+                TypeNameHandling = TypeNameHandling.Auto,
+            });
+        }
+    }
+}
