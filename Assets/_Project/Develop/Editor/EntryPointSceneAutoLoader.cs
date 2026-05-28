9d4b88c8a9b99d8ae09d4149ac4995584555fc74
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-05-28 19:23:38 +0300

    chore: Add scene autoloader editor script

---
+++ Assets/_Project/Develop/Editor/EntryPointSceneAutoLoader.cs
@@ -0,0 +1,47 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public static class EntryPointSceneAutoLoader
+{
+    private const string MenuPath = "PlayFromBootstrap/Enabled";
+    private const string PlayFromBootstrapKey = "PlayFromBootstrapKey";
+
+    static EntryPointSceneAutoLoader()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            if (EditorPrefs.GetBool(PlayFromBootstrapKey) == false)
+            {
+                EditorSceneManager.playModeStartScene = null;
+                return;
+            }
+
+            if (EditorBuildSettings.scenes.Length == 0)
+                return;
+
+            EditorSceneManager.playModeStartScene = AssetDatabase
+                .LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
+        }
+    }
+
+
+    [MenuItem(MenuPath)]
+    private static void Toggle()
+    {
+        bool result = EditorPrefs.GetBool(PlayFromBootstrapKey);
+        EditorPrefs.SetBool(PlayFromBootstrapKey, !result);
+    }
+
+    [MenuItem(MenuPath, true)]
+    private static bool ToggleValidate()
+    {
+        Menu.SetChecked(MenuPath, EditorPrefs.GetBool(PlayFromBootstrapKey));
+        return true;
+    }
+}
