a087cc76badd137072c9a5bce8da5d071d8ee393
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-05-28 16:31:47 +0300

    feat: Add DataProvider abstraction and realization for PlayerData

---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagment/DataProviders/PlayerDataProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets._Project.Develop.Runtime.Utilities.DataManagment.DataProviders
+{
+    public class PlayerDataProvider : DataProvider<PlayerData>
+    {
+        public PlayerDataProvider(ISaveLoadService saveLoadService) : base(saveLoadService)
+        {
+        }
+
+        protected override PlayerData GetOriginData()
+        {
+            return new PlayerData()
+            {
+                //init Levels progression
+                //init Upgrades info
+            };
+        }
+    }
+}
