59d945dd983641977210990cc7f47500aede6776
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-05-28 14:19:00 +0300

    feat: Add meta base infrastructure

---
+++ Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuBootstrap.cs
@@ -0,0 +1,32 @@
+﻿using Assets._Project.Develop.Runtime.Infrastructure;
+using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Utilities.SceneManagment;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Meta.Infrastructure
+{
+    public class MainMenuBootstrap : SceneBootstrap
+    {
+        private DIContainer _container;
+
+        public override void ProcessRegistrations(DIContainer container, IInputSceneArgs inputArgs = null)
+        {
+            _container = container;
+
+            MainMenuContextRegistrations.Process(_container);
+        }
+
+        public override IEnumerator Initialize()
+        {
+            Debug.Log("Инициализация сцены главного меню");
+
+            yield break;
+        }
+
+        public override void Run()
+        {
+            Debug.Log("Старт сцены главного меню");
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuContextRegistrations.cs
@@ -0,0 +1,12 @@
+﻿using Assets._Project.Develop.Runtime.Infrastructure.DI;
+
+namespace Assets._Project.Develop.Runtime.Meta.Infrastructure
+{
+    public class MainMenuContextRegistrations
+    {
+        public static void Process(DIContainer container)
+        {
+
+        }
+    }
+}
