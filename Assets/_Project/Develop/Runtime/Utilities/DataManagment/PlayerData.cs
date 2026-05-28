bba9c692d0505ab283e5ec9d7c97403e52c56b54
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-05-28 15:56:18 +0300

    feat: Add SaveData marker and PlayerData realization

---
+++ Assets/_Project/Develop/Runtime/Utilities/DataManagment/PlayerData.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets._Project.Develop.Runtime.Utilities.DataManagment
+{
+    public class PlayerData : ISaveData
+    {
+        //Levels progression
+        //Upgrades info
+    }
+}
