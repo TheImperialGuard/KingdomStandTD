a1fc5f7fd3036368326b44b0088069a5d18eac4f
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-05-29 17:30:18 +0300

    feat: Add IShowableView interface

---
+++ Assets/_Project/Develop/Runtime/UI/Core/Views/IShowableView.cs
@@ -0,0 +1,11 @@
+﻿using DG.Tweening;
+
+namespace Assets._Project.Develop.Runtime.UI.Core.Views
+{
+    public interface IShowableView : IView
+    {
+        Tween Show();
+
+        Tween Hide();
+    }
+}
