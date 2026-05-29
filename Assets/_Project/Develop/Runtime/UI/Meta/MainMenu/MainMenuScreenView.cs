fc0450b59e8b0814e0ce51fe62594eba55103d82
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-05-29 19:01:37 +0300

    feat(UI): Add main menu screen and view script

---
+++ Assets/_Project/Develop/Runtime/UI/Meta/MainMenu/MainMenuScreenView.cs
@@ -0,0 +1,11 @@
+﻿using Assets._Project.Develop.Runtime.UI.Core.Views;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.UI.Meta.MainMenu
+{
+    public class MainMenuScreenView : MonoBehaviour, IView
+    {
+        [field: SerializeField] public List<RectTransform> LevelsPositionsList { get; private set; }
+    }
+}
