1b5500310f6b5020de306121c45f0db9adf469a3
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-02 19:17:10 +0300

    feat(UI): Add base for list views

---
+++ Assets/_Project/Develop/Runtime/UI/CommonViews/ElementsListView.cs
@@ -0,0 +1,27 @@
+﻿using Assets._Project.Develop.Runtime.UI.Core.Views;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.UI.CommonViews
+{
+    public class ElementsListView<TElement> : MonoBehaviour, IView where TElement : MonoBehaviour, IView
+    {
+        [SerializeField] private Transform _parent;
+
+        private List<TElement> _elements = new();
+
+        public IReadOnlyList<TElement> Elements => _elements;
+
+        public void AddElement(TElement element)
+        {
+            element.transform.SetParent(_parent);
+            _elements.Add(element);
+        }
+
+        public void RemoveElement(TElement element)
+        {
+            element.transform.SetParent(null);
+            _elements.Remove(element);
+        }
+    }
+}
