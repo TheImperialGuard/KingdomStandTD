37e897173738903751a49a8ededfc30977f93b63
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-12 18:20:58 +0300

    feat: Add common entity components

---
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/Common/CommonComponents.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Common
+{
+    public class RigidbodyComponent : IEntityComponent
+    {
+        public Rigidbody Value;
+    }
+
+    public class TransformComponent : IEntityComponent
+    {
+        public Transform Value;
+    }
+}
