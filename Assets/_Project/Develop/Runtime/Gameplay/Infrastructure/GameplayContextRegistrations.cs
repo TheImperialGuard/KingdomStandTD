a4a79ca709a6778d230dda7cb03f89b1c970c6ff
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-13 20:17:44 +0300

    feat: Add new services to gameplay context

--- Assets/_Project/Develop/Runtime/Gameplay/Infrastructure/GameplayContextRegistrations.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/Infrastructure/GameplayContextRegistrations.cs
@@ -1,4 +1,7 @@
-﻿using Assets._Project.Develop.Runtime.Infrastructure.DI;
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Utilities.AssetsManagment;
 
 namespace Assets._Project.Develop.Runtime.Gameplay.Infrastructure
 {
@@ -9,6 +12,31 @@ namespace Assets._Project.Develop.Runtime.Gameplay.Infrastructure
         public static void Process(DIContainer container, GameplayInputArgs args)
         {
             _inputArgs = args;
+
+            container.RegisterAsSingle(CreateEntitiesFactory);
+
+            container.RegisterAsSingle(CreateEntitiesLifeContext);
+
+            container.RegisterAsSingle(CreateMonoEntitiesFactory).NonLazy();
+
+            container.RegisterAsSingle(CreateCollidersRegistryService);
+        }
+
+        private static CollidersRegistryService CreateCollidersRegistryService(DIContainer c) => new();
+
+        private static EntitiesLifeContext CreateEntitiesLifeContext(DIContainer c) => new();
+
+        private static MonoEntitiesFactory CreateMonoEntitiesFactory(DIContainer c)
+        {
+            return new MonoEntitiesFactory(
+                c.Resolve<ResourcesAssetsLoader>(),
+                c.Resolve<EntitiesLifeContext>(),
+                c.Resolve<CollidersRegistryService>());
+        }
+
+        private static EntitiesFactory CreateEntitiesFactory(DIContainer c)
+        {
+            return new EntitiesFactory(c);
         }
     }
 }
