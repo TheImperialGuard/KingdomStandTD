b78cac2d4cfafcf73fcf31c0698804e4d76ca4fe
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-13 17:48:28 +0300

    feat: Add entities factory and life context

---
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/EntitiesFactory.cs
@@ -0,0 +1,26 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore
+{
+    public class EntitiesFactory
+    {
+        private readonly DIContainer _container;
+        private readonly EntitiesLifeContext _entitiesLifeContext;
+        private readonly CollidersRegistryService _collidersRegistryService;
+
+        private readonly MonoEntitiesFactory _monoEntitiesFactory;
+
+        public EntitiesFactory(DIContainer container)
+        {
+            _container = container;
+            _entitiesLifeContext = _container.Resolve<EntitiesLifeContext>();
+            _monoEntitiesFactory = _container.Resolve<MonoEntitiesFactory>();
+            _collidersRegistryService = _container.Resolve<CollidersRegistryService>();
+        }
+
+        private Entity CreateEmpty() => new Entity();
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/Mono/MonoEntity.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono
+{
+    public class MonoEntity : MonoBehaviour
+    {
+        private CollidersRegistryService _colllidersRegistryService;
+
+        private Entity _linkedEntity;
+
+        public Entity LinkedEntity => _linkedEntity;
+
+        public void Initialize(CollidersRegistryService collidersRegistryService)
+        {
+            _colllidersRegistryService = collidersRegistryService;
+        }
+
+        public void Link(Entity entity)
+        {
+            _linkedEntity = entity;
+
+            MonoEntityRegistrator[] registrators = GetComponentsInChildren<MonoEntityRegistrator>();
+
+            if(registrators != null )
+                foreach (MonoEntityRegistrator registrator in registrators)
+                    registrator.Register(entity);
+
+            EntityView[] views = GetComponentsInChildren<EntityView>();
+
+            if (views != null )
+                foreach (EntityView view in views)
+                    view.Link(entity);
+
+            foreach (Collider collider in GetComponentsInChildren<Collider>())
+                _colllidersRegistryService.Register(collider, entity);
+        }
+
+        public void Cleanup(Entity entity)
+        {
+            EntityView[] views = GetComponentsInChildren<EntityView>();
+
+            if (views != null)
+                foreach (EntityView view in views)
+                    view.Cleanup(entity);
+
+            foreach (Collider collider in GetComponentsInChildren<Collider>())
+                _colllidersRegistryService.Unregister(collider);
+        }
+    }
+}
