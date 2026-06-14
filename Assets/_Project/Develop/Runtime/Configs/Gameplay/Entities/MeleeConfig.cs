f115b3e27864bf6b6538d26ee42f43c6556ae39c
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-14 22:58:05 +0300

    feat: Add config class for melee

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/MeleeConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Entities/NewMeleeConfig", fileName = "MeleeConfig")]
+    public class MeleeConfig
+    {
+        [field: SerializeField] public string PrefabPath { get; private set; } = "Prefabs/Entities/Melee";
+        [field: SerializeField, Min(0)] public float MoveSpeed { get; private set; } = 5;
+        [field: SerializeField, Min(0)] public float RotateSpeed { get; private set; } = 900;
+    }
+}
