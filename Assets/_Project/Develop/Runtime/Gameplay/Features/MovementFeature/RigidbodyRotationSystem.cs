29b26f8126d9d2b213649a3ccc1dc7feefe52946
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-14 22:45:29 +0300

    feat: Add rigidbody move and rotate systems

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/MovementFeature/RigidbodyRotationSystem.cs
@@ -0,0 +1,48 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Systems;
+using Assets._Project.Develop.Runtime.Utilities.Conditions;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.MovementFeature
+{
+    public class RigidbodyRotationSystem : IInitializableSystem, IUpdatableSystem
+    {
+        private Rigidbody _rigidbody;
+
+        private ReactiveVariable<float> _rotationSpeed;
+
+        private ReactiveVariable<Vector3> _rotationDirection;
+
+        private ICompositeCondition _canRotate;
+
+        public void OnInit(Entity entity)
+        {
+            _rigidbody = entity.Rigidbody;
+            _rotationSpeed = entity.RotationSpeed;
+            _rotationDirection = entity.RotationDirection;
+
+            _canRotate = entity.CanRotate;
+
+            if (_rotationDirection.Value != Vector3.zero)
+                _rigidbody.transform.rotation = Quaternion.LookRotation(_rotationDirection.Value.normalized);
+        }
+
+        public void OnUpdate(float deltaTime)
+        {
+            if (_canRotate.Evaluate() == false)
+                return;
+
+            if (_rotationDirection.Value == Vector3.zero)
+                return;
+
+            Quaternion lookRotation = Quaternion.LookRotation(_rotationDirection.Value.normalized);
+
+            float step = _rotationSpeed.Value * deltaTime;
+
+            Quaternion rotation = Quaternion.RotateTowards(_rigidbody.rotation, lookRotation, step);
+
+            _rigidbody.MoveRotation(rotation);
+        }
+    }
+}
