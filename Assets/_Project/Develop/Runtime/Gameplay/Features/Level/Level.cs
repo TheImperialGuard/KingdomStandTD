6f56c34f9489c847cf2869a079656956ca150841
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-15 19:49:28 +0300

    feat: Change Level prefab and road path scripts

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/Level/Level.cs
@@ -0,0 +1,11 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.Features.LevelNavigation;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.Level
+{
+    public class Level : MonoBehaviour
+    {
+        [SerializeField] private List<RoadPath> _roadPaths;
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/LevelNavigation/RoadPath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.LevelNavigation
+{
+    [Serializable]
+    public class RoadPath
+    {
+        [SerializeField] private PathNumber _number;
+
+        [SerializeField] private Transform _spawner;
+
+        [SerializeField] private List<RoadTile> _tiles;
+
+        public Vector3 SpawnerPosition => _spawner.position;
+
+        public IReadOnlyList<RoadTile> Tiles => _tiles;
+
+        public PathNumber Number => _number;
+    }
+}
