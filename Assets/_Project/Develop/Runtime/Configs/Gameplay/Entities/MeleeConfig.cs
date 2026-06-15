cfaa27d6ab1409a5c3b70065788f60f29af3ed84
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-15 19:50:39 +0300

    fix: Add inheritance from ScriptableOblect for melee config

--- Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/MeleeConfig.cs
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/MeleeConfig.cs
@@ -3,7 +3,7 @@
 namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities
 {
     [CreateAssetMenu(menuName = "Configs/Gameplay/Entities/NewMeleeConfig", fileName = "MeleeConfig")]
-    public class MeleeConfig
+    public class MeleeConfig : ScriptableObject
     {
         [field: SerializeField] public string PrefabPath { get; private set; } = "Prefabs/Entities/Melee";
         [field: SerializeField, Min(0)] public float MoveSpeed { get; private set; } = 5;
