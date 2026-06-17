f0f7666dbc39e4abfdb70d02da2dbc7f0a75a661
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-17 17:28:02 +0300

    feat: Add Layers class and Triggers layer

---
+++ Assets/_Project/Develop/Runtime/Utilities/Layers.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Utilities
+{
+    public class Layers
+    {
+        public static readonly int Triggers = LayerMask.NameToLayer("Triggers");
+        public static readonly LayerMask TriggersMask = 1 << Triggers;
+    }
+}
