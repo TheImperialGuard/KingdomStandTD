9543749e172c049c61db255c4787b78901771eef
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-20 14:41:23 +0300

    feat: Add DI registrations for AI and navigation services

--- Assets/_Project/Develop/Runtime/Gameplay/Infrastructure/GameplayContextRegistrations.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/Infrastructure/GameplayContextRegistrations.cs
@@ -1,8 +1,12 @@
 ﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
 using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.EntitiesFactory;
 using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Gameplay.Features.AI.Brains;
+using Assets._Project.Develop.Runtime.Gameplay.Features.Enemies;
+using Assets._Project.Develop.Runtime.Gameplay.Features.Level;
 using Assets._Project.Develop.Runtime.Infrastructure.DI;
 using Assets._Project.Develop.Runtime.Utilities.AssetsManagment;
+using UnityEngine;
 
 namespace Assets._Project.Develop.Runtime.Gameplay.Infrastructure
 {
@@ -21,11 +25,21 @@ namespace Assets._Project.Develop.Runtime.Gameplay.Infrastructure
             container.RegisterAsSingle(CreateMonoEntitiesFactory).NonLazy();
 
             container.RegisterAsSingle(CreateCollidersRegistryService);
+
+            container.RegisterAsSingle(CreateLevelEnvironment).NonLazy();
+
+            container.RegisterAsSingle(CreateBrainsFactory);
+
+            container.RegisterAsSingle(CreateAIBrainsContext);
+
+            container.RegisterAsSingle(CreateEnemiesFactory);
         }
 
         private static CollidersRegistryService CreateCollidersRegistryService(DIContainer c) => new();
 
         private static EntitiesLifeContext CreateEntitiesLifeContext(DIContainer c) => new();
+
+        private static AIBrainsContext CreateAIBrainsContext(DIContainer c) => new();
 
         private static MonoEntitiesFactory CreateMonoEntitiesFactory(DIContainer c)
         {
@@ -36,8 +50,22 @@ namespace Assets._Project.Develop.Runtime.Gameplay.Infrastructure
         }
 
         private static EntitiesFactory CreateEntitiesFactory(DIContainer c)
+            => new(c);
+
+        private static EnemiesFactory CreateEnemiesFactory(DIContainer c)
+            => new(c);
+
+        private static BrainsFactory CreateBrainsFactory(DIContainer c)
+            => new(c);
+
+        private static Level CreateLevelEnvironment(DIContainer c)
         {
-            return new EntitiesFactory(c);
+            ResourcesAssetsLoader resourcesAssetsLoader = c.Resolve<ResourcesAssetsLoader>();
+
+            Level level = resourcesAssetsLoader
+                .Load<Level>("Prefabs/Levels/LevelOrigin");
+
+            return GameObject.Instantiate(level);
         }
     }
 }
