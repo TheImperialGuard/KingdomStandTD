666d30db645f976301943c148aecca0aefafaa9e
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-20 12:35:41 +0300

    feat: Add AI states for waypoint movement and rotation

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/AI/States/RotateToTargetState.cs
@@ -0,0 +1,30 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using Assets._Project.Develop.Runtime.Utilities.StateMachineCore;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.AI.States
+{
+    public class RotateToTargetState : State, IUpdatableState
+    {
+        private ReactiveVariable<Entity> _currentTarget;
+
+        private ReactiveVariable<Vector3> _rotationDirection;
+
+        private Transform _transform;
+
+        public RotateToTargetState(Entity entity)
+        {
+            _currentTarget = entity.CurrentTarget;
+            _rotationDirection = entity.RotationDirection;
+            _transform = entity.Transform;
+
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (_currentTarget != null)
+                _rotationDirection.Value = (_currentTarget.Value.Transform.position - _transform.position).normalized;
+        }
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/AI/States/WaypointsMovementState.cs
@@ -0,0 +1,39 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.Features.LevelNavigation;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using Assets._Project.Develop.Runtime.Utilities.StateMachineCore;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.AI.States
+{
+    public class WaypointsMovementState : State, IUpdatableState
+    {
+        private ReactiveVariable<Waypoint> _currentWaypoint;
+
+        private ReactiveVariable<Vector3> _movementDirection;
+        private ReactiveVariable<Vector3> _rotationDirection;
+
+        private Transform _transform;
+
+        private Vector3 _currentDirection;
+
+        public WaypointsMovementState(Entity entity)
+        {
+            _currentWaypoint = entity.CurrentWaypoint;
+            _movementDirection = entity.MoveDirection;
+            _rotationDirection = entity.RotationDirection;
+            _transform = entity.Transform;
+
+            _currentDirection = Vector3.zero;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (_currentWaypoint != null)
+                _currentDirection = (_currentWaypoint.Value.transform.position - _transform.position).normalized;
+
+            _movementDirection.Value = _currentDirection;
+            _rotationDirection.Value = _currentDirection;
+        }
+    }
+}
