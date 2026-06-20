3d7502f7d74d874ad4fc15d4734580303997a8cb
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-20 12:51:30 +0300

    feat: Add abstraction for entity configs

--- Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/MeleeConfig.cs
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/MeleeConfig.cs
@@ -3,7 +3,7 @@
 namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities
 {
     [CreateAssetMenu(menuName = "Configs/Gameplay/Entities/NewMeleeConfig", fileName = "MeleeConfig")]
-    public class MeleeConfig : ScriptableObject
+    public class MeleeConfig : EntityConfig
     {
         [field: SerializeField] public string PrefabPath { get; private set; } = "Prefabs/Entities/Melee";
         [field: SerializeField, Min(0)] public float MoveSpeed { get; private set; } = 5;
