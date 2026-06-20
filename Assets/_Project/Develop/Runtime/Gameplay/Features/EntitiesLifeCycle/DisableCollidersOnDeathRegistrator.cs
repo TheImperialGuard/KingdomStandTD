8728310d0bbf21135841106359cc7672f4d11094
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-20 15:53:09 +0300

    feat: Add colliders disable on death logic

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/EntitiesLifeCycle/DisableCollidersOnDeathRegistrator.cs
@@ -0,0 +1,17 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.EntitiesLifeCycle
+{
+    public class DisableCollidersOnDeathRegistrator : MonoEntityRegistrator
+    {
+        [SerializeField] private List<Collider> _colliders;
+
+        public override void Register(Entity entity)
+        {
+            entity.AddDisableCollidersOnDeath(_colliders);
+        }
+    }
+}
