d0ad008cf8d3bcfa5ede48131aa82e92813fc3fa
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-27 13:49:08 +0300

    feat: Add shoot components and shoot point support

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/Shoot/ShootComponents.cs
@@ -0,0 +1,25 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.Shoot
+{
+    public class ShootPoint : IEntityComponent
+    {
+        public Transform Value;
+    }
+
+    public class InstantShotDirections : IEntityComponent
+    {
+        public InstantShotDirectionArgsList Value;
+    }
+
+    public class InstantShootRange : IEntityComponent
+    {
+        public ReactiveVariable<float> Value;
+    }
+
+    public class IsProjectile : IEntityComponent
+    {
+    }
+}
