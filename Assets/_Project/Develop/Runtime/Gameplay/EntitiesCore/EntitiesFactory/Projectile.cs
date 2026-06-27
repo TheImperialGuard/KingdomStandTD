eb92aa6d5b170559d4302191204eb38ceba2685a
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-27 19:20:55 +0300

    feat: Add projectile creation logic

---
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/EntitiesFactory/Projectile.cs
@@ -0,0 +1,45 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.Features.MovementFeature;
+using Assets._Project.Develop.Runtime.Utilities.Conditions;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.EntitiesFactory
+{
+    public partial class EntitiesFactory
+    {
+        public Entity CreateProjectile(Vector3 position, Vector3 direction, float damage, Entity owner)
+        {
+            Entity entity = CreateEmpty();
+
+            _monoEntitiesFactory.Create(entity, position, "Prefabs/Entities/Projectiles/Projectile");
+
+            entity
+                .AddIsProjectile()
+                .AddOwner(new ReactiveVariable<Entity>(owner))
+                .AddMoveDirection(new ReactiveVariable<Vector3>(direction))
+                .AddMoveSpeed(new ReactiveVariable<float>(3f))
+                .AddIsMoving()
+                .AddRotationDirection(new ReactiveVariable<Vector3>(direction))
+                .AddRotationSpeed(new ReactiveVariable<float>(9999))
+                .AddIsDead();
+
+            ICompositeCondition canMove = new CompositeCondition()
+                .Add(new FuncCondition(() => entity.IsDead.Value == false));
+
+            ICompositeCondition canRotate = new CompositeCondition()
+                .Add(new FuncCondition(() => entity.IsDead.Value == false));
+
+            entity
+                .AddCanMove(canMove)
+                .AddCanRotate(canRotate);
+
+            entity
+                .AddSystem(new RigidbodyMovementSystem())
+                .AddSystem(new RigidbodyRotationSystem());
+
+            _entitiesLifeContext.Add(entity);
+
+            return entity;
+        }
+    }
+}
