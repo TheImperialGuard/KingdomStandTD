ce2dffbbcf8adf5adaf0c83eb344edba535d15a7
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-27 19:27:22 +0300

    feat: Add towers factory with test tower creation logic

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/Towers/TowersFactory.cs
@@ -0,0 +1,45 @@
+﻿using Assets._Project.Develop.Runtime.Configs.Gameplay.Towers;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.EntitiesFactory;
+using Assets._Project.Develop.Runtime.Gameplay.Features.AI.Brains;
+using Assets._Project.Develop.Runtime.Gameplay.Features.TargetSelecting;
+using Assets._Project.Develop.Runtime.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using System;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.Towers
+{
+    public class TowersFactory
+    {
+        private readonly DIContainer _container;
+
+        private readonly EntitiesFactory _entitiesFactory;
+        private readonly BrainsFactory _brainsFactory;
+
+        private readonly EntitiesLifeContext _entitiesLifeContext;
+
+        public TowersFactory(DIContainer container)
+        {
+            _container = container;
+
+            _entitiesFactory = _container.Resolve<EntitiesFactory>();
+            _brainsFactory = _container.Resolve<BrainsFactory>();
+
+            _entitiesLifeContext = _container.Resolve<EntitiesLifeContext>();
+        }
+
+        public Entity Create(Vector3 position, TowerConfig config)
+        {
+            Entity entity;
+
+            entity = _entitiesFactory.CreateTower(position, config);
+
+            _brainsFactory.CreateTowerBrain(entity, new NearestTargetInRangeSelector(entity));
+
+            _entitiesLifeContext.Add(entity);
+
+            return entity;
+        }
+    }
+}
