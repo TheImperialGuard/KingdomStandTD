c6596b64aa9e1cc6f319db58a96c4feb6774f4ef
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-27 14:09:59 +0300

    feat: Add abstraction for target selecting logic

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/TargetSelecting/ITargetSelector.cs
@@ -0,0 +1,10 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using System.Collections.Generic;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.TargetSelecting
+{
+    public interface ITargetSelector
+    {
+        Entity SelectTargetFrom(IEnumerable<Entity> targets);
+    }
+}
