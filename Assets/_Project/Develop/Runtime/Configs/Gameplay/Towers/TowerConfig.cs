d36681288e25308891f35e7c5b9d47f576eff135
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-27 13:50:18 +0300

    feat: Add base tower config

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Towers/TowerConfig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Towers
+{
+
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Towers/NewTowerConfig", fileName = "TowerConfig")]
+    public class TowerConfig : ScriptableObject
+    {
+        [field: SerializeField] public string PrefabPath { get; private set; }
+
+        [field: SerializeField, Min(0)] public float AttackRange { get; private set; } = 3f;
+
+        [field: SerializeField, Min(0)] public float AttackCooldown { get; private set; } = 1f;
+
+        [field: SerializeField, Min(0)] public float AttackDamage { get; private set; } = 2f;
+    }
+}
