551d8bb67f9086769f8cf24a4b2c36f060d8b305
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-29 17:11:21 +0300

    feat: Make projectile die by death mask

--- Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/EntitiesFactory/Projectile.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/EntitiesFactory/Projectile.cs
@@ -1,4 +1,7 @@
-﻿using Assets._Project.Develop.Runtime.Gameplay.Features.MovementFeature;
+﻿using Assets._Project.Develop.Runtime.Gameplay.Features.EntitiesLifeCycle;
+using Assets._Project.Develop.Runtime.Gameplay.Features.MovementFeature;
+using Assets._Project.Develop.Runtime.Gameplay.Features.Sensors;
+using Assets._Project.Develop.Runtime.Utilities;
 using Assets._Project.Develop.Runtime.Utilities.Conditions;
 using Assets._Project.Develop.Runtime.Utilities.Reactive;
 using UnityEngine;
@@ -21,7 +24,12 @@ namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.EntitiesFactory
                 .AddIsMoving()
                 .AddRotationDirection(new ReactiveVariable<Vector3>(direction))
                 .AddRotationSpeed(new ReactiveVariable<float>(9999))
-                .AddIsDead();
+                .AddIsDead()
+                .AddContactsDetectingMask(Layers.CharacterMask | Layers.EnviromentMask)
+                .AddContactCollidersBuffer(new Buffer<Collider>(64))
+                .AddContactEntitiesBuffer(new Buffer<Entity>(64))
+                .AddDeathMask(Layers.EnviromentMask)
+                .AddIsTouchDeathMask();
 
             ICompositeCondition canMove = new CompositeCondition()
                 .Add(new FuncCondition(() => entity.IsDead.Value == false));
@@ -29,13 +37,28 @@ namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.EntitiesFactory
             ICompositeCondition canRotate = new CompositeCondition()
                 .Add(new FuncCondition(() => entity.IsDead.Value == false));
 
+            ICompositeCondition mustDie = new CompositeCondition()
+                .Add(new FuncCondition(() => entity.IsTouchDeathMask.Value));
+
+            ICompositeCondition mustSelfRelease = new CompositeCondition(LogicOperations.Or)
+                .Add(new FuncCondition(() => entity.IsDead.Value))
+                .Add(new FuncCondition(() => entity.ContactEntitiesBuffer.Count >= 1));
+
             entity
                 .AddCanMove(canMove)
-                .AddCanRotate(canRotate);
+                .AddCanRotate(canRotate)
+                .AddMustDie(mustDie)
+                .AddMustSelfRelease(mustSelfRelease);
 
             entity
                 .AddSystem(new RigidbodyMovementSystem())
-                .AddSystem(new RigidbodyRotationSystem());
+                .AddSystem(new RigidbodyRotationSystem())
+                .AddSystem(new BodyContactsDetectingSystem())
+                .AddSystem(new BodyContactsEntitiesFilterSystem(_collidersRegistryService))
+                .AddSystem(new DeathMaskTouchDetectorSystem())
+                .AddSystem(new DeathSystem())
+                .AddSystem(new DisableCollidersOnDeathSystem())
+                .AddSystem(new SelfReleaseSystem(_entitiesLifeContext));
 
             _entitiesLifeContext.Add(entity);
 
