1da3753a2e1f64790e4ec49c2200c96be62565de
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-29 18:03:51 +0300

    feat: Add entities helper with deal damage method

---
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/EntitiesHelper.cs
@@ -0,0 +1,17 @@
+﻿using Assets._Project.Develop.Runtime.Utilities.Reactive;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore
+{
+    public class EntitiesHelper
+    {
+        public static bool TryTakeDamageFrom(Entity source, Entity target, float damage)
+        {
+            if (target.TryGetTakeDamageRequest(out ReactiveEvent<float> takeDamageRequest) == false)
+                return false;
+
+            takeDamageRequest.Invoke(damage);
+
+            return true;
+        }
+    }
+}
