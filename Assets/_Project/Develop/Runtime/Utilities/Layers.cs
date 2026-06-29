72f44bbd30e4c035418cf884c8746d5471fc3891
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-29 16:27:03 +0300

    feat: Add layer for enviroment

--- Assets/_Project/Develop/Runtime/Utilities/Layers.cs
+++ Assets/_Project/Develop/Runtime/Utilities/Layers.cs
@@ -6,5 +6,11 @@ namespace Assets._Project.Develop.Runtime.Utilities
     {
         public static readonly int Triggers = LayerMask.NameToLayer("Triggers");
         public static readonly LayerMask TriggersMask = 1 << Triggers;
+
+        public static readonly int Characters = LayerMask.NameToLayer("Characters");
+        public static readonly LayerMask CharacterMask = 1 << Characters;
+
+        public static readonly int Enviroment = LayerMask.NameToLayer("Enviroment");
+        public static readonly LayerMask EnviromentMask = 1 << Enviroment;
     }
 }
