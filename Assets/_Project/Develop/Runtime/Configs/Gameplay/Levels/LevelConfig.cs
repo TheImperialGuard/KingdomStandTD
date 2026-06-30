42fe6d3fd2687e46781ddc0ff1cebdc0d17636ce
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-06-30 16:51:41 +0300

    feat: Add level config and waves configs

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Levels/LevelConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Levels
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Levels/NewLevelConfig", fileName = "LevelConfig")]
+    public class LevelConfig : ScriptableObject
+    {
+        [field: SerializeField] public string PrefabPath {  get; private set; }
+
+        [field: SerializeField] public EnemiesWavesListConfig WavesConfig { get; private set; }
+    }
+}
