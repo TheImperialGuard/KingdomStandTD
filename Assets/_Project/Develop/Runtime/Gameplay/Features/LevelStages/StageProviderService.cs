1b021fb88e7fc823750f150cdca032d935be5472
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-07-02 19:22:49 +0300

    feat: Add stage provider service

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/LevelStages/StageProviderService.cs
@@ -0,0 +1,76 @@
+﻿using Assets._Project.Develop.Runtime.Configs.Gameplay.Levels;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.LevelStages
+{
+    public class StageProviderService : IDisposable
+    {
+        private StagesFactory _stagesFactory;
+
+        private List<StageConfig> _stagesConfigs;
+
+        private IStage _currentStage;
+
+        private ReactiveVariable<int> _currentStageNumber = new();
+        private ReactiveVariable<StageResults> _currentStageResult = new();
+
+        private IDisposable _stageEndedDisposable;
+
+        public StageProviderService(StagesFactory stagesFactory, List<StageConfig> stagesConfigs)
+        {
+            _stagesFactory = stagesFactory;
+            _stagesConfigs = new(stagesConfigs);
+        }
+
+        public IReadOnlyVariable<int> CurrentStageNumber => _currentStageNumber;
+
+        public IReadOnlyVariable<StageResults> CurrentStageResult => _currentStageResult;
+
+        public int StagesCount => _stagesConfigs.Count;
+
+        public bool HasNextStage() => _currentStageNumber.Value < StagesCount;
+
+        public void SwitchToNext()
+        {
+            if (HasNextStage() == false)
+                throw new InvalidOperationException();
+
+            if (_currentStage != null)
+                CleanupCurrent();
+
+            _currentStageNumber.Value++;
+            _currentStageResult.Value = StageResults.Uncompleted;
+
+            _currentStage = _stagesFactory.Create(_stagesConfigs[_currentStageNumber.Value - 1]);
+        }
+
+        public void StartCurrent()
+        {
+            _stageEndedDisposable = _currentStage.Completed.Subscribe(OnCurrentStageCompleted);
+
+            _currentStage.Start();
+        }
+
+        public void UpdateCurrent(float deltaTime) => _currentStage.Update(deltaTime);
+
+        public void CleanupCurrent() => _currentStage.Cleanup();
+
+        public void Dispose()
+        {
+            _currentStage?.Dispose();
+
+            _stageEndedDisposable?.Dispose();
+        }
+
+        private void OnCurrentStageCompleted()
+        {
+            _currentStageResult.Value = StageResults.Completed;
+        }
+    }
+}
