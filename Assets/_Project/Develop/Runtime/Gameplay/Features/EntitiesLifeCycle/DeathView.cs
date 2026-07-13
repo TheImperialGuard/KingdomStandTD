d1a8665f63550614d4a537ea93caa675314e08c9
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-07-13 13:56:38 +0300

    chore: Import toon fx package

---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/EntitiesLifeCycle/DeathView.cs
@@ -0,0 +1,51 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.EntitiesLifeCycle
+{
+    [RequireComponent(typeof(Animator))]
+    public class DeathView : EntityView
+    {
+        [SerializeField] private Animator _animator;
+
+        [SerializeField] private string _isDeadKeyKeyName;
+
+        private IReadOnlyVariable<bool> _isDead;
+
+        private IDisposable _isDeadChangedDisposable;
+
+        private int IsDeadKey => Animator.StringToHash(_isDeadKeyKeyName);
+
+        private void OnValidate()
+        {
+            _animator = _animator != null ? _animator : GetComponent<Animator>();
+        }
+
+        public override void Cleanup(Entity entity)
+        {
+            base.Cleanup(entity);
+
+            _isDeadChangedDisposable.Dispose();
+        }
+
+        protected override void OnEntityStartedWork(Entity entity)
+        {
+            _isDead = entity.IsDead;
+
+            _isDeadChangedDisposable = _isDead.Subscribe(OnIsDeadChanged);
+
+            UpdateDeadView(_isDead.Value);
+        }
+
+        private void OnIsDeadChanged(bool oldIsMoving, bool isMoving) => UpdateDeadView(isMoving);
+
+        private void UpdateDeadView(bool value) => _animator.SetBool(IsDeadKey, value);
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/MovementFeature/RunningView.cs
@@ -0,0 +1,47 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
+using System;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.MovementFeature
+{
+    [RequireComponent(typeof(Animator))]
+    public class RunningView : EntityView
+    {
+        [SerializeField] private Animator _animator;
+
+        [SerializeField] private string _isRunningKeyName;
+
+        private IReadOnlyVariable<bool> _isMoving;
+
+        private IDisposable _isMovingChangedDisposable;
+
+        private int IsRunningKey => Animator.StringToHash(_isRunningKeyName);
+
+        private void OnValidate()
+        {
+            _animator = _animator != null ? _animator : GetComponent<Animator>();
+        }
+
+        public override void Cleanup(Entity entity)
+        {
+            base.Cleanup(entity);
+
+            _isMovingChangedDisposable.Dispose();
+        }
+
+        protected override void OnEntityStartedWork(Entity entity)
+        {
+            _isMoving = entity.IsMoving;
+
+            _isMovingChangedDisposable = _isMoving.Subscribe(OnIsMovingChanged);
+
+            UpdateRunningView(_isMoving.Value);
+        }
+
+        private void OnIsMovingChanged(bool oldIsMoving, bool isMoving) => UpdateRunningView(isMoving);
+
+        private void UpdateRunningView(bool value) => _animator.SetBool(IsRunningKey, value);
+    }
+}
