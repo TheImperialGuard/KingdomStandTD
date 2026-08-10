678c722308a6aa4cf10cd39cf59ca7e460612f33
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-08-10 13:23:58 +0300

    feat: Add config for additional target ability

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Abilities/AdditionalShootingTargetsAbilityConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Abilities
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Abilities/AdditionalShootingTargetsAbilityConfig", fileName = "AdditionalShootingTargetsAbilityConfig")]
+    public class AdditionalShootingTargetsAbilityConfig : AbilityConfig
+    {
+        [SerializeField] private List<int> _levelsCosts;
+
+        [SerializeField] private int _additionalTargetPerLevel;
+
+        public override int MaxLevel => _levelsCosts.Count;
+
+        public int GetAdditionsTargetsBy(int level) => level * _additionalTargetPerLevel;
+    }
+}
