a719c97f3d76a5cc4243c10f63ececa18847b75a
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-08-10 13:25:18 +0300

    feat: Add config for royal arrows tower

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/Towers/RoyalArrowsTowerConfig.cs
@@ -0,0 +1,18 @@
+﻿using Assets._Project.Develop.Runtime.Configs.Gameplay.Abilities;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities.Towers
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Entities/Towers/NewRoyalArrowsTowerConfig", fileName = "RoyalArrowsTowerConfig")]
+    public class RoyalArrowsTowerConfig : TowerConfig
+    {
+        [field: SerializeField, Min(0)] public float ProjectileSpeed { get; private set; } = 6f;
+
+        [SerializeField] private List<AbilityConfig> _firstAbility;
+        [SerializeField] private List<AbilityConfig> _secondAbility;
+
+        public IReadOnlyList<AbilityConfig> FirstAbility => _firstAbility;
+        public IReadOnlyList<AbilityConfig> SecondAbility => _secondAbility;
+    }
+}
