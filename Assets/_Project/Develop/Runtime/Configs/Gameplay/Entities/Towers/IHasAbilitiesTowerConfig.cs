b532488ae2190def546decd336b7d060fcc817ef
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-08-12 14:48:36 +0300

    refactor: Add interface for towers with abilities

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/Towers/IHasAbilitiesTowerConfig.cs
@@ -0,0 +1,11 @@
+﻿using Assets._Project.Develop.Runtime.Configs.Gameplay.Abilities;
+using System.Collections.Generic;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities.Towers
+{
+    public interface IHasAbilitiesTowerConfig
+    {
+        IReadOnlyList<AbilityConfig> FirstAbilityGroup { get; }
+        IReadOnlyList<AbilityConfig> SecondAbilityGroup { get; }
+    }
+}
--- Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/Towers/RoyalArrowsTowerConfig.cs
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/Towers/RoyalArrowsTowerConfig.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities.Towers
 {
     [CreateAssetMenu(menuName = "Configs/Gameplay/Entities/Towers/NewRoyalArrowsTowerConfig", fileName = "RoyalArrowsTowerConfig")]
-    public class RoyalArrowsTowerConfig : TowerConfig
+    public class RoyalArrowsTowerConfig : TowerConfig, IHasAbilitiesTowerConfig
     {
         [field: SerializeField, Min(0)] public float ProjectileSpeed { get; private set; } = 6f;
 
         [SerializeField] private List<AbilityConfig> _firstAbility;
         [SerializeField] private List<AbilityConfig> _secondAbility;
 
-        public IReadOnlyList<AbilityConfig> FirstAbility => _firstAbility;
-        public IReadOnlyList<AbilityConfig> SecondAbility => _secondAbility;
+        public IReadOnlyList<AbilityConfig> FirstAbilityGroup => _firstAbility;
+        public IReadOnlyList<AbilityConfig> SecondAbilityGroup => _secondAbility;
     }
 }
