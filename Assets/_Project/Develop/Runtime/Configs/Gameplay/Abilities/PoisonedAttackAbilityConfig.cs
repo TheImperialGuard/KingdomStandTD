ae2db36dc1aa2eff6d483326c429ac3bb9b6f244
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-08-12 16:54:00 +0300

    feat: Add config for poisoned attack ability

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Abilities/PoisonedAttackAbilityConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Abilities
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Abilities/PoisonedAttackAbilityConfig", fileName = "PoisonedAttackAbilityConfig")]
+    public class PoisonedAttackAbilityConfig : AbilityConfig
+    {
+        [SerializeField] private List<float> _poisonDamagePerSecondByLevel;
+        [SerializeField] private List<float> _poisonEffectTimeByLevel;
+
+        public float GetDamagePerSecondByLevel(int level) => _poisonDamagePerSecondByLevel[level - 1];
+        public float GetEffectTimeByLevel(int level) => _poisonEffectTimeByLevel[level - 1];
+    }
+}
