50bb8327dd9c1dfcc3151f84eaf0d03e73a81e35
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-08-17 15:24:01 +0300

    feat: Add config for magic tower level 4

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/Towers/HighMagicTowerConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities.Towers
+{
+    public class HighMagicTowerConfig
+    {
+    }
+}
