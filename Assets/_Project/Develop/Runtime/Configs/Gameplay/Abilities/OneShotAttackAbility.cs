7eaaa56f36d60713bb1db89113826e86a80ccf91
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-08-17 16:43:36 +0300

    feat: Add oneshoot ability config

---
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Abilities/OneShotAttackAbility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Abilities
+{
+    [CreateAssetMenu(menuName = "Configs/Gameplay/Abilities/OneShotAttackAbility", fileName = "OneShotAttackAbility")]
+    public class OneShotAttackAbility : AbilityConfig
+    {
+        [SerializeField] private List<float> _cooldownPerLevel;
+
+        public float GetCooldownBy(int level) => _cooldownPerLevel[level - 1];
+    }
+}
