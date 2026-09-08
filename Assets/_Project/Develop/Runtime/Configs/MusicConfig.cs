18ad5d51a3156cf3055a90f0502f0953f0d01032
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-09-08 13:46:19 +0300

    feat: Add music config

---
+++ Assets/_Project/Develop/Runtime/Configs/MusicConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Configs
+{
+    [CreateAssetMenu(menuName = "Configs/MusicConfig", fileName = "MusicConfig")]
+    public class MusicConfig : ScriptableObject
+    {
+        [field: SerializeField] public AudioClip MainMenuClip { get; private set; }
+        [field: SerializeField] public AudioClip GameplayPrepareClip { get; private set; }
+        [field: SerializeField] public AudioClip GameplayBattleClip { get; private set; }
+        [field: SerializeField] public AudioClip GameplayBossfightClip { get; private set; }
+        [field: SerializeField] public AudioClip GameplayWinClip { get; private set; }
+        [field: SerializeField] public AudioClip GameplayDefeatClip { get; private set; }
+    }
+}
