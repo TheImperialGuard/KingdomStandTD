35c52aaaca63a66a2bf89133be389eb1d335f072
Smirnov_Ivan <ivan.smirnov.dm@gmail.com>
2026-09-12 16:52:10 +0300

    feat: Add new loading screen view

---
+++ Assets/_Project/Develop/Runtime/Utilities/CameraManagment/SceneScreenshot.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Utilities.CameraManagment
+{
+    public class SceneScreenshot : MonoBehaviour
+    {
+        [SerializeField] private Camera targetCamera;
+        [SerializeField] private int width = 3840;
+        [SerializeField] private int height = 2160;
+
+        [ContextMenu("Take Screenshot")]
+        public void TakeScreenshot()
+        {
+            if (targetCamera == null)
+                targetCamera = Camera.main;
+
+            RenderTexture renderTexture = new RenderTexture(
+                width,
+                height,
+                24,
+                RenderTextureFormat.ARGB32
+            );
+
+            Texture2D screenshot = new Texture2D(
+                width,
+                height,
+                TextureFormat.RGB24,
+                false
+            );
+
+            RenderTexture previousTarget = targetCamera.targetTexture;
+            RenderTexture previousActive = RenderTexture.active;
+
+            targetCamera.targetTexture = renderTexture;
+            RenderTexture.active = renderTexture;
+
+            targetCamera.Render();
+
+            screenshot.ReadPixels(
+                new Rect(0, 0, width, height),
+                0,
+                0
+            );
+
+            screenshot.Apply();
+
+            targetCamera.targetTexture = previousTarget;
+            RenderTexture.active = previousActive;
+
+            byte[] pngData = screenshot.EncodeToPNG();
+            string path = Path.Combine(
+                Application.dataPath,
+                "..",
+                "SceneScreenshot.png"
+            );
+
+            File.WriteAllBytes(path, pngData);
+
+            Debug.Log($"Скриншот сохранён: {Path.GetFullPath(path)}");
+
+            DestroyImmediate(screenshot);
+            renderTexture.Release();
+            DestroyImmediate(renderTexture);
+        }
+    }
+}
